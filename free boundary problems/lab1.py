e57870aca3c5d21f8380867bc890c6847119c62a
egorzezyulko <egor.zezyulko@mail.ru>
2024-06-03 14:19:21 +0300

    added logics but wrong

--- free boundary problems/lab1.py
+++ free boundary problems/lab1.py
@@ -34,23 +34,28 @@ def condition():
     return False
 
 def RSweepMethod(_r):
-    Bo = 1
-    alpha = np.pi / 4
+    Bo = 10
+    alpha = np.pi / 3
+    eps = 1e-7
 
     h = _r[1] - _r[0]
     n = len(_r)
     A = np.zeros((n, n))
     d = np.zeros(n)
     z = np.zeros(n)
-    z[1: -1] = _r[1:-1] - 1 # начальное
+    z = _r - 1 # начальное
+
     
     A[-2, -2] = -4 / (2 * h)
     A[-2, -3] = 1 / (2 * h)
     d[-2] = np.tan(alpha)
     
+    counter = 1
+
     while True:
-        temp = z
+        temp = np.array(z)
         integ = I(_r, temp)
+        print(integ)
 
         d[1:-2] = _r[1:-2] * (- Bo * integ**(1/3) / np.pi + 2 * np.sin(alpha))
         A[1, 1] = b(1, _r, temp, Bo, integ) + 4 / 3 * a(1, _r, temp)
@@ -61,16 +66,49 @@ def RSweepMethod(_r):
             A[i, i - 1] = a(i, _r, temp)
             A[i, i] = b(i, _r, temp, Bo, integ)
             A[i, i + 1] = c(i, _r, temp)
-
-        break
-
+        
     
+        Alp = np.zeros(n) # коэффициент альфа для прогонки
+        Bet = np.zeros(n) 
+        Alp[2] = - A[1, 2] / A[1, 1]
+        Bet[2] = d[1] / A[1, 1]
+        # прогонка
+        for i in range(2, n - 2):
+            Alp[i + 1] = - A[i, i + 1] / (A[i, i] - A[i, i - 1] * Alp[i])
+            Bet[i + 1] = (d[i] - A[i, i - 1] * Bet[i]) / (A[i, i] - A[i, i - 1] * Alp[i])
+        
+        temp[n - 2] = (d[n - 2] - A[n - 2, n - 3] * Bet[n - 2]) / (A[n - 2, n - 2] - A[n - 2, n - 2] * Alp[n - 2])
 
+        for i in range(n - 3, 1, -1):
+            temp[i] = Alp[i + 1] * temp[i + 1] + Bet[i + 1]
+        
+        temp[0] = 4 / 3 * temp[1] - 1 / 3 * temp[2]
+        #print(temp)
+
+        if (max(np.abs(temp - z)) < eps):
+            z = temp
+            break
+
+        counter += 1
+        z = temp
+    print(counter)
+
+    return z
+
+def plot(_r, _z):
+    plt.plot(_r, _z)
+    plt.show()
 
 def main():
-    N = 10
+    N = 100
     r = np.linspace(0, 1, N)
-    RSweepMethod(r)
+    z = RSweepMethod(r)
+
+    #integ = I(r, z)
+    #r /= (integ**(1/3))
+    #z /= (integ**(1/3))
+
+    plot(r, z)
 
 if __name__ == "__main__":
     main()
