00ea9cba609bc729b8196b9a835061f649fd07ce
egorzezyulko <egor.zezyulko@mail.ru>
2024-06-04 03:52:47 +0300

    vrode i kachestvennee versiya no odin her ne rabotaet

--- free boundary problems/lab1.py
+++ free boundary problems/lab1.py
@@ -28,15 +28,12 @@ def b(_ind, _r, _z, _Bo, _I):
 
 def c(_ind, _r, _z):
     h = _r[1] - _r[0]
-    return k(_ind + 1, _r, _z) / h**2
-
-def condition():
-    return False
+    return k(_ind + 1, _r, _z) / h**2 
 
 def RSweepMethod(_r):
-    Bo = 10
-    alpha = np.pi / 3
-    eps = 1e-7
+    Bo = 1
+    alpha = np.pi / 6
+    eps = 1e-4
 
     h = _r[1] - _r[0]
     n = len(_r)
@@ -44,11 +41,6 @@ def RSweepMethod(_r):
     d = np.zeros(n)
     z = np.zeros(n)
     z = _r - 1 # начальное
-
-    
-    A[-2, -2] = -4 / (2 * h)
-    A[-2, -3] = 1 / (2 * h)
-    d[-2] = np.tan(alpha)
     
     counter = 1
 
@@ -57,40 +49,44 @@ def RSweepMethod(_r):
         integ = I(_r, temp)
         print(integ)
 
-        d[1:-2] = _r[1:-2] * (- Bo * integ**(1/3) / np.pi + 2 * np.sin(alpha))
-        A[1, 1] = b(1, _r, temp, Bo, integ) + 4 / 3 * a(1, _r, temp)
-        A[1, 2] = c(1, _r, temp) - 1 / 3 * a(1, _r, temp)
+        d[0] = h**2 / 2 * (-Bo * integ**(1/3) / np.pi + 2 * np.sin(alpha))
+        d[1:-2] = -_r[1:-2] * (Bo * integ**(1/3) / np.pi - 2 * np.sin(alpha))
+        d[-2] = -h * np.tan(alpha) - h**2 / (2 * np.cos(alpha)**3) * (Bo * integ**(1/3) / np.pi - np.sin(alpha))
+
+        A[0, 0] = h**2 * Bo / (2*integ**(2/3)) - 1
+        A[0, 1] = 1
+        A[-2, -2] = 1
 
         # заполнение оставшейся части матрицы
-        for i in range(2, n - 2):
+        for i in range(1, n - 2):
             A[i, i - 1] = a(i, _r, temp)
             A[i, i] = b(i, _r, temp, Bo, integ)
             A[i, i + 1] = c(i, _r, temp)
         
+        print(A)
     
-        Alp = np.zeros(n) # коэффициент альфа для прогонки
-        Bet = np.zeros(n) 
-        Alp[2] = - A[1, 2] / A[1, 1]
-        Bet[2] = d[1] / A[1, 1]
+        Alp = np.zeros(n) # коэффициенты альфа для прогонки
+        Bet = np.zeros(n) # коэффициенты бета для прогонки
+
+        Alp[1] = -A[0, 1] / A[0, 0]
+        Bet[1] = d[0] / A[0, 0]
+
         # прогонка
-        for i in range(2, n - 2):
+        for i in range(1, n - 1):
             Alp[i + 1] = - A[i, i + 1] / (A[i, i] - A[i, i - 1] * Alp[i])
             Bet[i + 1] = (d[i] - A[i, i - 1] * Bet[i]) / (A[i, i] - A[i, i - 1] * Alp[i])
         
-        temp[n - 2] = (d[n - 2] - A[n - 2, n - 3] * Bet[n - 2]) / (A[n - 2, n - 2] - A[n - 2, n - 2] * Alp[n - 2])
+        temp[n - 2] = Bet[n - 1]
 
-        for i in range(n - 3, 1, -1):
+        for i in range(n - 3, -1, -1):
             temp[i] = Alp[i + 1] * temp[i + 1] + Bet[i + 1]
-        
-        temp[0] = 4 / 3 * temp[1] - 1 / 3 * temp[2]
-        #print(temp)
 
         if (max(np.abs(temp - z)) < eps):
-            z = temp
+            z = np.array(temp)
             break
 
         counter += 1
-        z = temp
+        z = np.array(temp)
     print(counter)
 
     return z
@@ -100,13 +96,13 @@ def plot(_r, _z):
     plt.show()
 
 def main():
-    N = 100
+    N = 20
     r = np.linspace(0, 1, N)
     z = RSweepMethod(r)
 
-    #integ = I(r, z)
-    #r /= (integ**(1/3))
-    #z /= (integ**(1/3))
+    integ = I(r, z)
+    r /= (integ**(1/3))
+    z /= (integ**(1/3))
 
     plot(r, z)
 
